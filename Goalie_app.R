7e1c204996a116dd07f3e377a71212cd774f199d
Ryan Dajani <39835415+RyanD17@users.noreply.github.com>
2024-08-18 17:29:23 -0400

    Add files via upload

---
+++ Goalie_app.R
@@ -0,0 +1,46 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+source("C:/Users/ryand/Dropbox/Sports Analytics/hockey_data/Utah_HC_project/goalie_analysis.R")
+
+goalie_stats <- read.csv("goalie_stats_csv")
+
+head(goalie_stats)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Goalie Analysis"),
+    
+    
+
+    
+   
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
